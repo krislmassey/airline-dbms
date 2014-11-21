6ec538a37589a0301eeeb4bffbb37b9c55d1f687
krislmassey <kristenmassey@yahoo.com>
2014-11-20 22:04:30 -0600

    ER Diagram final draft

--- populate.sql
+++ populate.sql
@@ -168,18 +168,18 @@ VALUES('5','5');
 
 
 INSERT INTO Seats
-VALUES('1','','','1','1','Y');
+VALUES('1','1','1','1','1','1');
 
 INSERT INTO Seats
-VALUES('2','','','1','1','Y');
+VALUES('2','1','1','1','1','1');
 
 INSERT INTO Seats
-VALUES('3','','','1','1','Y');
+VALUES('3','1','1','1','1','1');
 
 INSERT INTO Seats
-VALUES('4','','','1','1','Y');
+VALUES('4','1','1','1','1','1');
 
 INSERT INTO Seats
-VALUES('5','','','1','1','Y');
+VALUES('5','1','1','1','1','1');
 
 
